1e90a88ec1223c0111971af9243d901596b5da2c
Jens Nordenbro <jnb@beijer.se>
2018-11-28 12:55:22 +0100

    posix syntax

--- Docker-Compose/DockerBuild.sh
+++ Docker-Compose/DockerBuild.sh
@@ -1,4 +1,4 @@
-cp -r ..\ServerServerProxy .
+cp -r ../ServerServerProxy .
 docker build . -t serverserver:latest -f Dockerfile.serverserver 
 docker build . -t nginx-vnc-cloud:latest -f Dockerfile.nginx-vnc-cloud
 docker build . -t novnc:latest -f Dockerfile.novnc
