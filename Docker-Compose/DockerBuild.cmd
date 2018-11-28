59b25939562f8fc11bc87d150e7bd3cc53d3db9d
Jens Nordenbro <jnb@beijer.se>
2018-11-28 11:22:05 +0100

    changes dockerbuild

--- Docker-Compose/DockerBuild.cmd
+++ Docker-Compose/DockerBuild.cmd
@@ -1,3 +1,3 @@
-docker build -t serverserver:latest Dockerfile.serverserver 
-docker build -t nginx-vnc-cloud:latest Dockerfile.nginx-vnc-cloud
-docker build -t novnc:latest Dockerfile.novnc
+docker build -t serverserver:latest -f ockerfile.serverserver 
+docker build -t nginx-vnc-cloud:latest -f Dockerfile.nginx-vnc-cloud
+docker build -t novnc:latest -f Dockerfile.novnc
